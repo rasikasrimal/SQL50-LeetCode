b63a58c73837534eea3760c426049a2f5bea9ec2
Rasika srimal <77890327+rasikasrimal@users.noreply.github.com>
2023-07-16 07:29:12 +0530

    Update and rename 595. Big Countries to 595. Big Countries.sql

--- 595. Big Countries.sql
+++ 595. Big Countries.sql
@@ -1,4 +1,4 @@
-# Write your MySQL query statement below
+-- Write your MySQL query statement below
 SELECT name,population,area from World where area >= 3000000 or population >= 25000000;
 
 #Another way - USING UNION KEYWORD
