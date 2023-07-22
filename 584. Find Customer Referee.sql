6518a7329719506efc091367ec0c91e7b01ec1e7
Rasika srimal <77890327+rasikasrimal@users.noreply.github.com>
2023-07-22 08:29:41 +0530

    Update 584. Find Customer Referee.sql
    Learned new way to simplify where clause (22nd July)

--- 584. Find Customer Referee.sql
+++ 584. Find Customer Referee.sql
@@ -1,2 +1,7 @@
 -- Write your MySQL query statement below
 SELECT name FROM customer where referee_id IS NULL OR referee_id != 2;
+
+# 2nd Method
+# WHERE COALESCE(referee_id, -1) <> 2; 
+# This condition will return rows where the referee_id is not equal to 2 or NULL. 
+# If referee_id is NULL, it will be treated as -1 in the comparison.
